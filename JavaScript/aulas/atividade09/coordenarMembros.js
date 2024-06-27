d98d28bffd4a7e0cbb16acb6d444eed65ce9a6b5
Eduardo-J-S <eduardojose59@hotmail.com>
2024-06-26 23:53:44 -0300

    feat: adicionando id aos membros para melhor identificação

--- JavaScript/aulas/atividade09/coordenarMembros.js
+++ JavaScript/aulas/atividade09/coordenarMembros.js
@@ -7,81 +7,85 @@ let rl = readline.createInterface({
 
 let sistemaCoordenar = [
   {
+    membro_id: 1,
     membro: "nomeMembro",
     tarefa: ["tarefa 1", "tarefa 2"],
   },
   {
+    membro_id: 2,
     membro: "outroMembro",
     tarefa: ["tarefa 3", "tarefa 4"],
   },
 ];
 
 const membros = () => {
-  rl.question("Digite o nome do membro a ser adcionado: ", (nome) => {
+  rl.question("Digite o id do membro a ser adcionado: ", (id) => {
     let membroCorrespondente = sistemaCoordenar.find(
-      (membros) => membros.membro === nome
+      (membros) => membros.membro_id === parseFloat(id)
     );
-    if (!membroCorrespondente) {
-      sistemaCoordenar.push({ membro: nome, tarefa: [] });
-      console.log("Membro cadastrado com sucesso!");
-    } else {
-      console.log("Membro já cadastrado");
+    
+    if (membroCorrespondente) {
+      console.log(`Membro com o id ${id} já cadastrado`);
+      console.log(`Nome do membro: ${membroCorrespondente.membro}`)
+      menuMembros();
     }
-    menuMembros();
+    rl.question("Digite o nome do membro a ser adcionado: ", (nome) => {
+      sistemaCoordenar.push({ membro_id: parseInt(id), membro: nome, tarefa: [] });
+      console.log("Membro cadastrado com sucesso!");
+
+      menuMembros();
+    });
   });
 };
 
 const tarefas = () => {
   rl.question(
-    "Digite o nome do membro a quem quer atribuir uma tarefa: ", (nome) => {
+    "Digite o id do membro a quem quer atribuir uma tarefa: ", (id) => {
+      let membroCorrespondente = sistemaCoordenar.find(
+        (membros) => membros.membro_id === parseInt(id)
+      );
+
+      if (!membroCorrespondente) {
+        console.log("Nenhum membro com esse id encontrado.");
+        menuMembros();
+      }
       rl.question(
-        `Digite o tarefa que quer atribuir ao membro ${nome}: `, (descricao) => {
-          let membroCorrespondente = sistemaCoordenar.find(
-            (membros) => membros.membro === nome
+        `Digite o tarefa que quer atribuir ao membro de nome ${membroCorrespondente.membro}: `, (descricao) => {
+          
+          let tarefaJaDesignada = sistemaCoordenar.some((membro) =>
+            membro.tarefa.some((tar) => tar === descricao)
           );
 
-          //Aqui membroCorrespondenteFilter é um array, não um objeto único
-          //Tentar acessar .tarefa diretamente causará um erro
-          //let membroCorrespondenteFilter = sistemaCoordenar.find(membros => membros.membro === nome);  Erro: membroCorrespondenteFilter.tarefa.push is not a function
-
-          if (membroCorrespondente) {
-
-            let tarefaJaDesignada = sistemaCoordenar.some(membro =>
-              membro.tarefa.some(tar => tar === descricao)
-            );
-            
-            if (tarefaJaDesignada) {
-              console.log("Tarefa já designada a um membro.");
-            } else {
-              membroCorrespondente.tarefa.push(descricao);
-              console.log(membroCorrespondente);
-              console.log("Tarefa designada com sucesso a um membro.");
-            }
-
-            // let interromper = false;
-
-            // for (let i = 0; i < sistemaCoordenar.length; i++) {
-            //   for (let j = 0; j < sistemaCoordenar[i].tarefa.length; j++) {
-            //     let tar = sistemaCoordenar[i].tarefa[j];
-            //     if (tar === descricao) {
-            //       interromper = true;
-            //       break;
-            //     }
-            //   }
-            //   if (interromper) {
-            //     console.log("Tarefa já designada a um membro.");
-            //     break;
-            //   }
-            // }
-
-            // if (!interromper) {
-            //   membroCorrespondente.tarefa.push(descricao);
-            //   console.log(membroCorrespondente);
-            //   console.log("Tarefa designada com sucesso a um membro.");
-            // }
+          if (tarefaJaDesignada) {
+            console.log("Tarefa já designada a um membro.");
           } else {
-            console.log("Nenhum membro com esse nome encontrado.");
+            membroCorrespondente.tarefa.push(descricao);
+            console.log(membroCorrespondente);
+            console.log("Tarefa designada com sucesso a um membro.");
           }
+
+          // let interromper = false;
+
+          // for (let i = 0; i < sistemaCoordenar.length; i++) {
+          //   for (let j = 0; j < sistemaCoordenar[i].tarefa.length; j++) {
+          //     let tar = sistemaCoordenar[i].tarefa[j];
+          //     if (tar === descricao) {
+          //       interromper = true;
+          //       break;
+          //     }
+          //   }
+          //   if (interromper) {
+          //     console.log("Tarefa já designada a um membro.");
+          //     break;
+          //   }
+          // }
+
+          // if (!interromper) {
+          //   membroCorrespondente.tarefa.push(descricao);
+          //   console.log(membroCorrespondente);
+          //   console.log("Tarefa designada com sucesso a um membro.");
+          // }
+
           menuMembros();
         }
       );
@@ -92,9 +96,9 @@ const tarefas = () => {
 const listarMembros = () => {
   if (sistemaCoordenar.length > 0) {
     for (let i = 0; i < sistemaCoordenar.length; i++) {
-      console.log(`Membro: ${sistemaCoordenar[i].membro} - Tarefas: ${sistemaCoordenar[i].tarefa.length > 0 ? 
-        sistemaCoordenar[i].tarefa : "nenhuma tarefa atribuida."
-      }`);
+      console.log(
+        `ID Membro ${sistemaCoordenar[i].membro_id} - Membro: ${sistemaCoordenar[i].membro} - Tarefas: ${sistemaCoordenar[i].tarefa.length > 0 ?
+          sistemaCoordenar[i].tarefa : "nenhuma tarefa atribuida."}`);
     }
   } else {
     console.log("Nenhum membro encontrado.");
