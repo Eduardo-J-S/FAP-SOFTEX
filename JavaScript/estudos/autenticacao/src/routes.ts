2c39028aba04aa8a177e52d214ee169417c04ade
Eduardo-J-S <eduardojose59@hotmail.com>
2024-09-19 23:44:08 -0300

    add api authentication

---
+++ JavaScript/estudos/autenticacao/src/routes.ts
@@ -0,0 +1,15 @@
+import { Router } from "express";
+import { UserController } from "./controller/UserController";
+import { AuthController } from "./controller/AuthController";
+import { AuthMiddlewares } from "./middlewares/auth";
+
+const userController = new UserController();
+const authController = new AuthController();
+
+export const router = Router();
+
+router.post("/create", userController.store)
+
+router.get("/users", AuthMiddlewares, userController.index)
+
+router.post("/auth", authController.authenticate)
